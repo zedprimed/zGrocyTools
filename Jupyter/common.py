b823b3673e631f47a317bc091304e90d62fca4a5
zedprimed <samuel.r.lau@gmail.com>
2024-08-25 21:51:52 -0500

    Common Updates and README Start
    Add series timezone handling to Common
    Add Google API to Common
    Start outlining readme with a quickstart

--- Jupyter/common.py
+++ Jupyter/common.py
@@ -1,5 +1,16 @@
 import requests
 import json
+import common
+import os
+import pandas as pd
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import google.auth
 
 # Create class for global config
 class GrConf:
@@ -10,22 +21,128 @@ class GrConf:
         #class decs
         self.apikey = config['LocalUser']['APIKey']
         self.entrypoint = config['LocalUser']['URL Root']
-        self.objects = config['Readpoints']['Objects']
+        self.interface = config['Interface']
         self.tz = config['LocalUser']['Timezone']
+        global CONF
+        CONF = self
 
 # Create class for API calls
 class GrGetAPI:
-    def __init__(self,config,obj):
-        self.endpoint = config.entrypoint+config.objects[obj]
-        self.key = config.apikey
+    def __init__(self,category,obj):
+        self.endpoint = CONF.entrypoint+CONF.interface[category][obj]['path']
+        self.key = CONF.apikey
         self.headers = {
             "accept": "application/json",
-            "GROCY-API-KEY": self.key}
+            "GROCY-API-KEY": self.key} # What headers for Put or Post?
         self.params = {}
         self.r = 0
+        self.reference = CONF.interface[category][obj]
     def buildParam(self,*query):
 
         self.params = {"query[]":query}
         
-    def call(self):
-        self.r = requests.get(self.endpoint,headers=self.headers,params=self.params)
+    def get(self):
+        if bool(self.reference['get']):
+            self.r = requests.get(self.endpoint,headers=self.headers,params=self.params)
+        else:
+            self.r = "Failure: get not enabled for this API"
+
+# Create a function for timezone handling of pandas series
+def tz(series):
+    series = pd.to_datetime(series,utc=True)
+    if CONF.tz:
+        series = series.dt.tz_convert(CONF.tz)
+    return series
+
+# Google reference functions
+#Oauth
+
+def GoogleOAuth():
+    SCOPES = ["https://www.googleapis.com/auth/drive.file",]
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    #Instead of file token.json, get config token
+    localConfigR = json.load(open("localEnv.json","r"))
+    if localConfigR['GoogleAPI']['token'] != "":
+        creds = Credentials.from_authorized_user_info(localConfigR['GoogleAPI']['token'], SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            credentials = localConfigR['GoogleAPI']['credentials']
+            flow = InstalledAppFlow.from_client_config(
+                credentials, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    #creds.to_json() did not appear to work right
+    cold = json.loads(creds.to_json())
+    localConfigR['GoogleAPI']['token'] = cold
+    new = open("localEnv.json","w")
+    json.dump(localConfigR,new)
+    new.close()
+    return creds  
+# Create a sheet
+
+def create(title,creds):
+  """
+  Creates the Sheet the user has access to.
+  Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  """
+  #creds, _ = google.auth.default() #turn off ADC for now???
+  # pylint: disable=maybe-no-member
+  try:
+    service = build("sheets", "v4", credentials=creds)
+    spreadsheet = {"properties": {"title": title}}
+    spreadsheet = (
+        service.spreadsheets()
+        .create(body=spreadsheet, fields="spreadsheetId")
+        .execute()
+    )
+    print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
+    return spreadsheet.get("spreadsheetId")
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    return error
+
+
+if __name__ == "__main__":
+  # Pass: title
+  create("mysheet1")
+
+#Read a sheet
+def spreadsheet_get(creds,spreadsheet_id, range_name=''):
+  """
+  Creates the batch_update the user has access to.
+  Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  """
+  #creds, _ = google.auth.default()
+  # pylint: disable=maybe-no-member
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    result = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range=range_name)
+        .execute()
+    )
+    rows = result.get("values", [])
+    print(f"{len(rows)} rows retrieved")
+    return result
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    return error
+
+
+if __name__ == "__main__":
+  # Pass: spreadsheet_id, and range_name
+  get_values("1CM29gwKIzeXsAppeNwrc8lbYaVMmUclprLuLYuHog4k", "A1:C2")
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # zGrocyTools
- Grocy Web Tools (JS based?)
+A Python learning project to use Grocy as a small data platform
+## Quick Start
+1. [Install Python 3](https://www.python.org/downloads/) (at time of documentation this project has been using Python 3.12.5)
+2. PIP dependency modules:
+	1. [pandas](https://pandas.pydata.org/docs/getting_started/install.html): pip install pandas
+	2. [Google Client Library](https://developers.google.com/sheets/api/quickstart/python): pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+	3. [Interactive Tables](https://github.com/mwouts/itables): pip install itables
+3. Install Jupyter Lab
+	* Option 1: [As a PIP download]()
+	* Option 2: [As a desktop client](https://github.com/jupyterlab/jupyterlab-desktop)
+	* Option 3: [As a Docker stack](https://jupyter-docker-stacks.readthedocs.io/en/latest/index.html) or other self-hosted server
+4. Download Jupyter folder from main repo to a place of your choosing. Be aware your choice of Jupyet Lab install will control what is in scope of your Lab
+5. Copy exmplEnv.json to a new file: localEnv.json and fill in the local config options:
+	1. GoogleAPI-Credentials: Copy and paste the contents of a Google Cloud project (with the Sheets API activated) OAuth Client ID secret JSON download.
+	2. LocalUser-APIKey - a Grocy client generated API key for the user you wish to transact with the Grocy server with the permissions expected to be used (i.e. change mode if you are expecting to utilize mass change scripts, or only read modes if you just want mass download scripts)
+	3. LocalUser-Timezone - a pytz timezone to use as local time assuming Grocy config of UTC server time.
+	4. LocalUser-URL Root - The HTTP address to your Grocy server ending in the API point /api/
