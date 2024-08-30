2827c036b5eed52877540cf8f37dc678363e0c17
zedprimed <samuel.r.lau@gmail.com>
2024-08-30 11:13:59 -0500

    Google API and DDIC
    Prep work for spreadsheet handling:
    Google sheet bootstrapper to use limited Google security scope
    start adding google methods and classes for calling the sheets API
    start of a DDIC for handling types properly as we change from pandas to sheets to api calls and any direction in between

--- Jupyter/common.py
+++ Jupyter/common.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import common
 import os
 import pandas as pd
 
@@ -38,9 +37,12 @@ class GrGetAPI:
         self.r = 0
         self.reference = CONF.interface[category][obj]
     def buildParam(self,*query):
-
-        self.params = {"query[]":query}
-        
+        for x in query:
+            #param is in Grocy query statements
+            self.params['query[]'] = x
+    def buildFreeParam(self,param):
+        #param is in request Dict
+        self.params.update(param)
     def get(self):
         if bool(self.reference['get']):
             self.r = requests.get(self.endpoint,headers=self.headers,params=self.params)
@@ -89,60 +91,181 @@ def GoogleOAuth():
 # Create a sheet
 
 def create(title,creds):
-  """
-  Creates the Sheet the user has access to.
-  Load pre-authorized user credentials from the environment.
-  TODO(developer) - See https://developers.google.com/identity
-  for guides on implementing OAuth2 for the application.
-  """
-  #creds, _ = google.auth.default() #turn off ADC for now???
-  # pylint: disable=maybe-no-member
-  try:
-    service = build("sheets", "v4", credentials=creds)
-    spreadsheet = {"properties": {"title": title}}
-    spreadsheet = (
-        service.spreadsheets()
-        .create(body=spreadsheet, fields="spreadsheetId")
-        .execute()
-    )
-    print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
-    return spreadsheet.get("spreadsheetId")
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return error
-
-
-if __name__ == "__main__":
-  # Pass: title
-  create("mysheet1")
+    """
+    Creates the Sheet the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    #creds, _ = google.auth.default() #turn off ADC for now???
+    # pylint: disable=maybe-no-member
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        spreadsheet = {"properties": {"title": title}}
+        spreadsheet = (
+                service.spreadsheets()
+                .create(body=spreadsheet, fields="spreadsheetId")
+                .execute()
+        )
+        print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
+        return spreadsheet.get("spreadsheetId")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+    if __name__ == "__main__":
+            # Pass: title
+            create("mysheet1")
 
 #Read a sheet
 def spreadsheet_get(creds,spreadsheet_id, range_name=''):
-  """
-  Creates the batch_update the user has access to.
-  Load pre-authorized user credentials from the environment.
-  TODO(developer) - See https://developers.google.com/identity
-  for guides on implementing OAuth2 for the application.
-  """
-  #creds, _ = google.auth.default()
-  # pylint: disable=maybe-no-member
-  try:
+    """
+    Creates the batch_update the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    #creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        result = (
+                service.spreadsheets()
+                .values()
+                .get(spreadsheetId=spreadsheet_id, range=range_name)
+                .execute()
+        )
+        rows = result.get("values", [])
+        print(f"{len(rows)} rows retrieved")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
+#Update a sheet
+# sheets_batch_update - utilizes batching functionality to limit API calls. Not sure I'll need it?
+# sheets_append - simply appends a list to the requested sheet
+
+def sheets_append(creds,spreadsheet_id,sheetname,data):
     service = build("sheets", "v4", credentials=creds)
-
-    result = (
-        service.spreadsheets()
-        .values()
-        .get(spreadsheetId=spreadsheet_id, range=range_name)
-        .execute()
+    response = (
+                service.spreadsheets()
+                .values()
+                .append(
+                    spreadsheetId = spreadsheet_id,
+                    range=f'{sheetname}!A1',
+                    valueInputOption="USER_ENTERED",
+                    body = {
+                            'values': data,
+                            })
+                .execute()
     )
-    rows = result.get("values", [])
-    print(f"{len(rows)} rows retrieved")
-    return result
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return error
+    return response
+
+'''
+Leaving this for reference - I've implemented a class handler containing this as a call method to match my other REST calls
+def sheets_batch_update(creds,spreadsheet_id, title='', find='', replacement='', append=''):
+    """
+    Update the sheet details in batch, the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+
+    #creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        requests = []
+        # Change the spreadsheet's title.
+        if title != '':
+            requests.append(
+                    {
+                            "updateSpreadsheetProperties": {
+                                    "properties": {"title": title},
+                                    "fields": "title",
+                            }
+                    }
+            )
+        # Find and replace text
+        if find != '' and replacement != '':
+            requests.append(
+                    {
+                            "findReplace": {
+                                    "find": find,
+                                    "replacement": replacement,
+                                    "allSheets": True,
+                            }
+                    }
+            )
+        #Append requests
+        
+        # Add additional requests (operations) ...
 
 
-if __name__ == "__main__":
-  # Pass: spreadsheet_id, and range_name
-  get_values("1CM29gwKIzeXsAppeNwrc8lbYaVMmUclprLuLYuHog4k", "A1:C2")
+        if requests == []:
+            print('No ops to do?!')
+            return 'Noop'
+        body = {"requests": requests}
+        response = (
+                service.spreadsheets()
+                .batchUpdate(spreadsheetId=spreadsheet_id, body=body)
+                .execute()
+        )
+        return response
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+'''
+
+class GoBatchUpdate:
+    def __init__(self,creds,spreadsheet_id):
+        self.creds = creds
+        self.spreadsheet_id = spreadsheet_id
+        self.requests = []
+
+    def addReq(self,req):
+        self.requests.append(req)
+    
+    def call(self):
+        """
+        Update the sheet details in batch, the user has access to.
+        Load pre-authorized user credentials from the environment.
+        TODO(developer) - See https://developers.google.com/identity
+        for guides on implementing OAuth2 for the application.
+    
+        requests is a list of JSON requests in dict format 
+        ex.
+        [{
+    "addSheet": {
+        "properties": {
+            "title": "APISHeet"
+            }
+        }
+        }]        
+        """
+    
+        #creds, _ = google.auth.default()
+        # pylint: disable=maybe-no-member
+        creds = self.creds
+        spreadsheet_id = self.spreadsheet_id
+        requests = self.requests
+        try:
+            service = build("sheets", "v4", credentials=creds)
+            if not len(requests) > 0:
+                print('No ops to do?!')
+                return 'Noop'
+            body = {"requests": requests}
+            print(body)
+            response = (
+                    service.spreadsheets()
+                    .batchUpdate(spreadsheetId=spreadsheet_id, body=body)
+                    .execute()
+            )
+            self.r = response
+        except HttpError as error:
+            #print(f"An error occurred: {error}")
+            self.r = error
--- README.md
+++ README.md
@@ -7,12 +7,21 @@ A Python learning project to use Grocy as a small data platform
 	2. [Google Client Library](https://developers.google.com/sheets/api/quickstart/python): pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 	3. [Interactive Tables](https://github.com/mwouts/itables): pip install itables
 3. Install Jupyter Lab
-	* Option 1: [As a PIP download]()
+	* Option 1: [As a PIP download](https://jupyter.org/install): pip install jupyterlab
 	* Option 2: [As a desktop client](https://github.com/jupyterlab/jupyterlab-desktop)
 	* Option 3: [As a Docker stack](https://jupyter-docker-stacks.readthedocs.io/en/latest/index.html) or other self-hosted server
-4. Download Jupyter folder from main repo to a place of your choosing. Be aware your choice of Jupyet Lab install will control what is in scope of your Lab
+4. Download Jupyter folder from main repo to a place of your choosing. This should be somewhere your Jupyter Lab has access to (influenced by how you installed it)
 5. Copy exmplEnv.json to a new file: localEnv.json and fill in the local config options:
 	1. GoogleAPI-Credentials: Copy and paste the contents of a Google Cloud project (with the Sheets API activated) OAuth Client ID secret JSON download.
 	2. LocalUser-APIKey - a Grocy client generated API key for the user you wish to transact with the Grocy server with the permissions expected to be used (i.e. change mode if you are expecting to utilize mass change scripts, or only read modes if you just want mass download scripts)
-	3. LocalUser-Timezone - a pytz timezone to use as local time assuming Grocy config of UTC server time.
+	3. LocalUser-Timezone - a pytz timezone to use as local time assuming Grocy config of UTC server time. Run the command below in a Python environment (ex. the Jupyter Lab console) to get the current list supported by the module.
+	
+	```
+	import pytz
+	pytz.all_timezones
+	```
 	4. LocalUser-URL Root - The HTTP address to your Grocy server ending in the API point /api/
+	
+6. Import the Workspaces files from the Jupyter/Workspaces folder to populate your Lab with the default workspaces. You can then access lab directly by the workspace and get into a view that's ready to do what you want.
+
+## Roadmap
